c48f5d574e5239d898c7396b499f99c0126c19b5
Reatlilwe Maroga <rea.maroga@gmail.com>
2024-08-09 17:05:34 +0200

    add icons and descriptions from api

--- src/index.js
+++ src/index.js
@@ -9,11 +9,43 @@ function refreshWeather(response){
     let countryElement = document.querySelector("#country");
     countryElement.innerHTML = response.data.country;
 
-    let humidityElement = document.querySelector("#")
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
 
+    let dayElement = document.querySelector("#day");
+    let date = new Date(response.data.time * 1000);
+    dayElement.innerHTML = formatDate(date);
 
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
 }
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    return `${day} ${hours}:${minutes}`;
+  }
 
 function searchCity(city){
  //make api call and update the app
