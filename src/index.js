2685b1bae7e1ca1be81eb0599029554742f81861
Reatlilwe Maroga <rea.maroga@gmail.com>
2024-08-11 18:23:17 +0200

    add forecast feature

--- src/index.js
+++ src/index.js
@@ -24,7 +24,9 @@ function refreshWeather(response){
 
     let descriptionElement = document.querySelector("#description");
     descriptionElement.innerHTML = response.data.condition.description;
-}
+
+    getForecast(response.data.city);
+  }
 
 function formatDate(date) {
     let minutes = date.getMinutes();
@@ -62,6 +64,50 @@ function handleSearchSubmit(event){
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let days = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu','Fri', 'Sat'];
+
+  return days[date.getDay()];
+}
+
+
+function getForecast(city){
+  let apiKey = `7f9d9cf0474030cet59a45f7coc640b0`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+}
+
+
+
+
+function displayForecast(response) {
+
+
+let forecastHtml = "";
+
+response.data.daily.forEach(function (day, index) {
+  if (index < 5){
+    forecastHtml = forecastHtml + ` 
+    <div class="forecast-day">
+    <div class="forecast-date">${formatDay(day.time)}</div>
+
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    <div class="forecast-temperatures">
+        <div class="forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+        </div>
+        <div class="forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
+    </div>
+    </div>
+    `;
+  }
+    });
+
+
+let forecastElement = document.querySelector('#forecast');
+forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 
